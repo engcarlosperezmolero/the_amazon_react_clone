fec2657ceb75f1f120d59da3b4a72dda21e62aae
CarlosPerezMolero <perez.moleroc@gmail.com>
2020-10-18 02:44:41 -0300

    init set up and header componente ui

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyArIOTAQT97GXGmCiPPKNTU5HAj4Z2lMJg",
+  authDomain: "the-ammazon-clone.firebaseapp.com",
+  databaseURL: "https://the-ammazon-clone.firebaseio.com",
+  projectId: "the-ammazon-clone",
+  storageBucket: "the-ammazon-clone.appspot.com",
+  messagingSenderId: "835299424593",
+  appId: "1:835299424593:web:79eecb5c4030ef1ab9bb70",
+  measurementId: "G-GFPZTL4976"
+};
